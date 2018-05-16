8f4f1a5c210cc3faab4fc62d24e6af8cfeb5e131
Volodymyr Boichentsov <vladimirb@3d4medical.com>
2018-05-16 12:49:23 +0100

    Revert "set path"
    This reverts commit f3fb801f892520fea0ff4f87f468bf3573041cfc.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "glTFSceneKit",
-            path: "Sources/glTFSceneKit",
             dependencies: []),
         .testTarget(
             name: "glTFSceneKitTests",
